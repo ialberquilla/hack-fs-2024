6ac9998c1e6174c855a390cb12cfdb868380e137
Ivan <ivan.alberquilla@gmail.com>
2024-05-29 18:04:04 +0200

    send results to IPFS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,49 @@
 import { WebSocket } from "ws";
+import dotenv from "dotenv";
+import { FleekSdk, PersonalAccessTokenService } from "@fleekxyz/sdk";
 
-const ws = new WebSocket.Server({ port: 3100 }, 
-    ()=> console.log('Server started')
+dotenv.config();
+
+const personalAccessToken = process.env.PAT_IPFS || "";
+const projectId = process.env.PROJECT_ID_IPFS;
+
+const newAccessTokenService = new PersonalAccessTokenService({
+  personalAccessToken,
+  projectId,
+});
+
+const fleekSdk = new FleekSdk({ accessTokenService: newAccessTokenService });
+
+export const uploadToIPFS = async (filename: string, content: Buffer) => {
+  const result = await fleekSdk.ipfs().add({
+    path: filename,
+    content: content,
+  });
+
+  return result;
+};
+
+const ws = new WebSocket.Server({ port: 3100 }, () =>
+  console.log("Server started")
 );
 
-ws.on('connection', (socket: any) => {
-    console.log('Client connected');
-    socket.on('message', (msg: Buffer) => {
-        const msgStr = msg.toString();
-        const msgJson = JSON.parse(msgStr);
-        
-        console.log('Received message:', msgJson);
+ws.on("connection", (socket: any) => {
+  console.log("Client connected");
+  socket.on("message", (msg: Buffer) => {
+    const msgStr = msg.toString();
+    console.log("Received message:", msgStr);
+    socket.send("Echo: " + msgStr);
 
-        socket.send('Echo: ' + msgStr);
-    });
+    const user = msgStr.split(":")[0];
+    const score = msgStr.split(":")[1];
+    const timestamp = new Date().getTime();
+
+    uploadToIPFS(`${user}-${score}-${timestamp}.json`, Buffer.from(msgStr))
+      .then((result) => {
+        console.log("Uploaded to IPFS:", result);
+      })
+      .catch((err) => {
+        console.error("Error uploading to IPFS:", err);
+      });
+  });
 });
