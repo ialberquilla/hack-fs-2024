bb35669d4d4be5e9bbb4d70f93a7e977c9a51688
Ivan <ivan.alberquilla@gmail.com>
2024-05-29 20:00:40 +0200

    fix lint

--- dapp/packages/nextjs/app/scores/page.tsx
+++ dapp/packages/nextjs/app/scores/page.tsx
@@ -48,8 +48,8 @@ const Debug: NextPage = () => {
               </tr>
             </thead>
             <tbody>
-              {files.map((file: any) => (
-                <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+              {files.map((file: any, index: number) => (
+                <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                   <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {'Amanzing track'}
                   </th>
